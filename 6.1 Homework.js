3255c1763287b064cd5ff50bed838876e67484d7
Oksana <deminaon@gmail.com>
2023-04-26 17:10:52 +1000

    6.4 Homework

--- 6.1 Homework.js
+++ 6.1 Homework.js
@@ -18,7 +18,19 @@ function getRestAfterDivision(x, y) {
   return x;
 }
 
+function Increment(x) {
+  x++;
+  return x;
+}
+
+function Decrement(x) {
+  x--;
+  return x;
+}
+
 console.log(increaseBalance(3000, 700));
 console.log(decreaseBalance(3000, 700));
 console.log(divideBalanceByAccounts(3000, 2));
 console.log(getRestAfterDivision(7000, 3));
+console.log(Increment(4));
+console.log(Decrement(10));
